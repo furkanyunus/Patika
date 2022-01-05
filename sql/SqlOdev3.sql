326dd9f346c5d65e788d14e7909b68caa0a56559
Furkan Yunus <tangut_furkan@hotmail.com>
2022-01-05 22:07:49 +0300

    Odev3

---
+++ sql/SqlOdev3.sql
@@ -0,0 +1,6 @@
+
+select * from country where country LIKE 'A%a'
+select * from country where country LIKE '_____n'
+select title from film where title  LIKE '%T%'
+select * from title LIKE 'C%' AND  
+(length>90 and rental_rate=2.99)
